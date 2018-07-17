143ee9865fbe43a813064011401b00a9da516653
frozenvoice <frozenvoice83@gmail.com>
2018-07-17 17:57:48 +0900

    test

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
-app.get('/', function (req, res){
-  res.send('Hi~~');
-});
-
+// app.get('/', function (req, res){
+//   res.send('Hi~~');
+// });
+app.use(express.static(path.join(__dirname, '/public')));
+console.log(__dirname);
 app.listen(3000, function() {
-  console.log('Server on~~');
+  console.log('Server on!');
 });
